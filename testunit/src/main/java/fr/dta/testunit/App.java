51d816e1dad1390f088a2b99ee829e39991ec8ef
BriceB <brice.beteille@gmail.com>
2019-10-28 10:39:15 +0100

    Project creation to test the unit test method for programming

---
+++ testunit/src/main/java/fr/dta/testunit/App.java
@@ -0,0 +1,13 @@
+package fr.dta.testunit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
